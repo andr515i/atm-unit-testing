74bbd2fba76dcbf5746c67bf141f0807721da1ab
Andreas <andr515i@zbc.dk>
2024-02-05 14:42:52 +0100

    Add project files.

---
+++ Hæveautomaten.tests/AccountTests.cs
@@ -0,0 +1,16 @@
+﻿using Hæveautomaten;
+using Xunit;
+namespace Hæveautomaten.tests
+{
+	public class AccountTests
+	{
+
+		[Theory]
+		[InlineData(10, 5, 5)]
+		[InlineData(10.5, 5.125, 5.375)]
+		public void SubtractTransaction_DecimalsShouldSubtract(decimal x, decimal y, decimal expceted)
+		{
+			decimal actual = Account.SubtractTransaction(x, y);
+		}
+	}
+}
---
+++ Hæveautomaten/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hæveautomaten
+{
+	public static class Account
+	{
+		public static decimal SubtractTransaction(decimal x, decimal y)
+		{
+			return x - y;
+		}
+	}
+}
